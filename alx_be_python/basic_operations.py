a9e240767a9bb6f74e9c356f7853562a9de82c06
JabaliMochache <mochachebyorder@gmail.com>
2025-07-19 21:26:40 +0300

    Fixed addition operation

---
+++ alx_be_python/basic_operations.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Basic Arithmetic Operations
+
+number1 = 10
+number2 = 5
+
+# CORRECTED OPERATIONS:
+print(f"Addition of {number1} and {number2} is {number1 + number2}")  # Fixed from * to +
+print(f"Subtraction of {number1} and {number2} is {number1 - number2}")
+print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
