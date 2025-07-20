4d43696bdca725cde37034aa5bffe26cc06a31de
JabaliMochache <mochachebyorder@gmail.com>
2025-07-20 21:20:50 +0300

    Add completed Python introduction scripts

---
+++ alx_be_python/python_introduction/future_age_calculator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Future Age Calculator
+
+# Get user input
+current_age = int(input("How old are you? "))
+
+# Calculate future age (2050 - 2023 = 27 years difference)
+future_age = current_age + 27
+
+# Print result
+print(f"In 2050, you will be {future_age} years old")
